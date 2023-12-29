75c3e89157ec4f95b1f8c59b9773c35b71aef8f0
Mehrbod_MK <Mehrbod_MK@DESKTOP-UL4OD0D>
2023-12-29 17:24:56 +0330

    Add project files.

---
+++ MehrbodMK_CompilerProject/MehrbodMK_CompilerProject.cpp
@@ -0,0 +1,24 @@
+// MehrbodMK_CompilerProject.cpp
+// Developer:  Mehrbod Molla Kazemi
+// Professor: Dr. Nasim Vatani
+// Date: 08 Dey 1402 (29 December 2023)
+
+// This project is a 'syntax-analyzer', one of the phases from the frontend of a compiler.
+
+// The grammer is this:
+/* 
+*  EXPR -> id = EXPR2 | id = NUMBER
+*  NUMBER -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+*  EXPR2 -> id | EXPR2 + | EXPR2 - | EXPR2 * | EXPR2 / | EXPR2 %
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "** MehrbodMK_CompilerProject.cpp **\n** Developer:  Mehrbod Molla Kazemi **\n** Professor: Dr. Nasim Vatani **\n** Date: 08 Dey 1402 (29 December 2023) **\n\n";
+
+    return 0;
+}
